bf11e4c6e59f0ec19f08e340946880d92ddae5cc
HackedOS <gmachina43@gmail.com>
2022-11-03 21:11:52 +0530

    fix collisions

--- player.gd
+++ player.gd
@@ -6,4 +6,4 @@ func _physics_process(delta):
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = velocity * 100
-	move_and_slide(velocity)
+	move_and_collide(velocity * delta)
