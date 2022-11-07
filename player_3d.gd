5d80fd636334d5454600fbcba34c2c33ca251fec
DualWield <28580319+OmegaLarmor@users.noreply.github.com>
2022-11-07 08:21:29 -0500

    Add more input mappings, cleanup

--- player_3d.gd
+++ player_3d.gd
@@ -1,13 +1,18 @@
 extends KinematicBody
 
+## Velocity multiplier for directions
+## Note that because of camera setup, using the same number results
+## in different visual results for x and z axis, so it's best to set them separately
+export(float, 1.0, 10.0) var _vel_horiz_mult = 5.0
+export(float, 1.0, 20.0) var _vel_z_mult = 10.0
+
 var velocity = Vector3.ZERO
 func _physics_process(_delta):
 	
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	velocity.z = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	velocity.z *= 10.0
-	velocity.x *= 5.0
+	velocity.x *= _vel_horiz_mult
+	velocity.z *= _vel_z_mult
 	
-	print(velocity)
 	var _unused = move_and_slide(velocity)
